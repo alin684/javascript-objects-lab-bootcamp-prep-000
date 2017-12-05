21f76eae7d032972e7eb2dd776bf4f3866373191
Abraham Lin <al684@cornell.edu>
2017-12-05 22:52:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newObject1 = Object.assign({}, object, {[key]:value});
   return newObject1;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+  var object = Object.assign(object, {[key:value}]});
+  return object
+}
